e8f9324bd2c96ede177ac52daa22d22726300fe7
Jamie Bryson <jamieab98@gmail.com>
2026-01-05 17:50:08 -0500

    completed the student_generator function

--- lib/data_generator.py
+++ lib/data_generator.py
@@ -1,8 +1,15 @@
 # This module contains functions to lazily generate student data.
+from student_data import students
 
 def student_generator(student_list, major):
+    student_records = (record for record in student_list if record[2].lower() == major.lower())
+    for record in student_records:
+        print(record)
+
     """
     Generate student records filtered by major lazily for memory efficiency
     using a Python generator.
     """
     pass
+
+student_generator(students, "Computer Science")
