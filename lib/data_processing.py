1184132f26f39bea17dfc1be245831afc5386d6e
Jamie Bryson <jamieab98@gmail.com>
2026-01-05 17:18:31 -0500

    completed the display_students functions

--- lib/data_processing.py
+++ lib/data_processing.py
@@ -4,7 +4,8 @@ from student_data import students
 def format_student_data(selected):
     selected_student = [student for student in students if student[1].lower() == selected.lower()]
     returned_string = f"ID: {selected_student[0][0]} | Name: {selected_student[0][1]} | Major: {selected_student[0][2]}"
-    return(returned_string)
+    print(returned_string)
+    return
     """
     Format student data for display.
     The function should return a formatted string containing:
@@ -16,10 +17,14 @@ def format_student_data(selected):
     pass
 
 def display_students(student_list):
+    for student in student_list:
+        student_name = student[1]
+        format_student_data(student_name)
+    return
     """
     Display all student records.
     Loop through the student_list and print each student using format_student_data().
     """
     pass
 
-format_student_data("Alice Johnson")
+display_students(students)
